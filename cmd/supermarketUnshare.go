81937f7fe26b464d3eb80808a6a83f56802dad93
nitin sanghi <er.nitin.sanghi@gmail.com>
2022-03-21 13:44:46 +0530

    Supermarket command completed
    Signed-off-by: nitin sanghi <er.nitin.sanghi@gmail.com>

--- cmd/supermarketUnshare.go
+++ cmd/supermarketUnshare.go
@@ -17,10 +17,11 @@ import (
 // supermarketSearchCmd represents the supermarket search
 var supermarketUnShareCmd = &cobra.Command{
 	Use:   "unshare",
-	Short: "Use the share argument to add a cookbook to Chef Supermarket.",
-	Long:  `Use the share argument to add a cookbook to Chef Supermarket. This action will require a user account and a certificate for Chef Supermarket. By default, knife will use the user name and API key that is identified in the configuration file used during the upload; otherwise these values must be specified on the command line or in an alternate configuration file.`,
+	Short: "Use the unshare argument to stop the sharing of a cookbook located at Chef Supermarket.",
+	Long:  `Use the unshare argument to stop the sharing of a cookbook located at Chef Supermarket. Only the maintainer of a cookbook may perform this action.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var ui core.UI
+		supermarket.ValidateArgsAndType(args, query, ui)
 		if !supermarket.ValidateArtifact(args[0]) {
 			ui.Msg("only cookbook type artifact supported as of now.")
 			os.Exit(1)
